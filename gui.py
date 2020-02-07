ebe30e98d107e2bf5328e896c4b1706260d6f6c0
NadaTheOptimist <58845919+NadaTheOptimist@users.noreply.github.com>
2020-02-07 16:43:06 +0100

    Add files via upload

--- gui.py
+++ gui.py
@@ -2,26 +2,50 @@ from tkinter import *
 from tkinter import messagebox as msg
 import subprocess
 
-root = Tk()
-root.title("BrainFrick IDE -1.0.2")
-codeBox = Text(root, height = 20, width = 80)
-programInput = Entry(root)
-def programCheck():
-    f = open("config.txt", 'r')
-    TimeOutLimit = int(f.read().split("\n")[0].split("=")[1])
-    f.close()
-    brainFrickCode = str(codeBox.get("1.0", END)).replace("\n", '')
-    brainFrickCodeInput = str(programInput.get())
-    try:
+"""
+try:
         output = subprocess.check_output("brainfrick.exe", input = (brainFrickCode + "\n" + brainFrickCodeInput).encode(), stderr = subprocess.STDOUT, timeout = TimeOutLimit)
     except subprocess.TimeoutExpired:
         msg.showerror("Error", "Your code ran for more than the timeout limit, and as such was shut down. Error x001, subprocess.TimeOutExpired")
+"""
+
+root = Tk()
+root.title("BrainFrick IDE -1.0.3")
+codeBox = Text(root, height = 20, width = 80)
+
+def programCheck():
+    brainFrickCode = str(codeBox.get("1.0", END))
+    # establishes a new Tkinter window for the program input
+    root = Tk()
+    root.title("Input window")
+    inputBox = Text(root, height = 40, width = 80)
+    def runProgram():
+        f = open("config.txt", 'r')
+        TimeOutLimit = int(f.read().split("\n")[0].split("=")[1])
+        f.close()
+
+        brainFrickCode = str(codeBox.get("1.0", END))
+        brainFrickCodeInput = str(inputBox.get("1.0", END))
+        try:
+            output = subprocess.check_output("brainfrick.exe", input = (brainFrickCode + "\n" + brainFrickCodeInput).encode(), stderr = subprocess.STDOUT, timeout = TimeOutLimit)
+        except subprocess.TimeoutExpired:
+            msg.showerror("Error", "Your code ran for more than the timeout limit, and as such was shut down. Error x001, subprocess.TimeOutExpired")
+        root = Tk()
+        root.title("Output window")
+        codeOutput = Text(root, height = 40, width = 80)
+        codeOutput.insert("1.0", output)
+        codeOutput.pack()
+        root.mainloop()
+    runButton = Button(root, text = "run", command = runProgram)
+    inputBox.pack()
+    runButton.pack()
+    root.mainloop()
     
-    msg.showinfo("RESULT", output.decode())
+
+   
 interpretButton = Button(root, text = "INTERPRET", command = programCheck)
 
 codeBox.pack()
-programInput.pack()
 interpretButton.pack()
 
 root.mainloop()
